270f6c05ecf4794d60633868a9d3e57a286bc4cf
DancingSnow <60736156+DancingSnow0517@users.noreply.github.com>
2024-09-05 06:54:22 +0800

    try update GTM to 1.4.0 (#20)
    * try update GTM to 1.4.0
    
    * spotless

--- src/main/java/com/epimorphismmc/monomorphism/recipe/MORecipeHelper.java
+++ src/main/java/com/epimorphismmc/monomorphism/recipe/MORecipeHelper.java
@@ -12,29 +12,35 @@ import net.minecraft.world.level.material.Fluid;
 
 public class MORecipeHelper {
 
-    public static Content itemContent(ItemStack itemStack, float chance, float tierChanceBoost) {
-        return new Content(ItemRecipeCapability.CAP.of(itemStack), chance, tierChanceBoost, null, null);
+    public static Content itemContent(
+            ItemStack itemStack, int chance, int maxChance, int tierChanceBoost) {
+        return new Content(
+                ItemRecipeCapability.CAP.of(itemStack), chance, maxChance, tierChanceBoost, null, null);
     }
 
-    public static Content itemContent(Item item, int amount, float chance, float tierChanceBoost) {
+    public static Content itemContent(
+            Item item, int amount, int chance, int maxChance, int tierChanceBoost) {
         return new Content(
                 ItemRecipeCapability.CAP.of(new ItemStack(item, amount)),
                 chance,
                 tierChanceBoost,
+                maxChance,
                 null,
                 null);
     }
 
-    public static Content fluidContent(FluidStack fluidStack, float chance, float tierChanceBoost) {
+    public static Content fluidContent(
+            FluidStack fluidStack, int chance, int maxChance, int tierChanceBoost) {
         return new Content(
-                FluidRecipeCapability.CAP.of(fluidStack), chance, tierChanceBoost, null, null);
+                FluidRecipeCapability.CAP.of(fluidStack), chance, maxChance, tierChanceBoost, null, null);
     }
 
     public static Content fluidContent(
-            Fluid fluid, long amount, float chance, float tierChanceBoost) {
+            Fluid fluid, long amount, int chance, int maxChance, int tierChanceBoost) {
         return new Content(
                 FluidRecipeCapability.CAP.of(FluidStack.create(fluid, amount)),
                 chance,
+                maxChance,
                 tierChanceBoost,
                 null,
                 null);
--- src/main/java/com/epimorphismmc/monomorphism/utility/MOTransferUtils.java
+++ src/main/java/com/epimorphismmc/monomorphism/utility/MOTransferUtils.java
@@ -3,15 +3,10 @@ package com.epimorphismmc.monomorphism.utility;
 import com.gregtechceu.gtceu.api.capability.recipe.IO;
 import com.gregtechceu.gtceu.api.capability.recipe.IRecipeHandler;
 import com.gregtechceu.gtceu.api.capability.recipe.RecipeCapability;
-import com.gregtechceu.gtceu.api.machine.trait.NotifiableItemStackHandler;
 import com.gregtechceu.gtceu.api.recipe.ingredient.FluidIngredient;
 import com.gregtechceu.gtceu.common.data.GTRecipeCapabilities;
 
-import com.lowdragmc.lowdraglib.misc.ItemTransferList;
 import com.lowdragmc.lowdraglib.side.fluid.FluidStack;
-import com.lowdragmc.lowdraglib.side.item.IItemTransfer;
-
-import net.minecraft.world.item.ItemStack;
 
 import com.google.common.collect.Table;
 
@@ -39,25 +34,5 @@ public class MOTransferUtils {
         }
 
         return stack;
-    }
-
-    @Deprecated(since = "gtm@1.3.2", forRemoval = true) // TODO
-    public static ItemStack extractItemAccountNotifiableList(
-            IItemTransfer handler, int slot, int amount, boolean simulate) {
-        if (handler instanceof ItemTransferList transferList) {
-            int index = 0;
-            for (var transfer : transferList.transfers) {
-                if (slot - index < transfer.getSlots()) {
-                    if (transfer instanceof NotifiableItemStackHandler notifiable) {
-                        return notifiable.extractItemInternal(slot - index, amount, simulate);
-                    } else {
-                        return transfer.extractItem(slot - index, amount, simulate);
-                    }
-                }
-                index += transfer.getSlots();
-            }
-            return ItemStack.EMPTY;
-        }
-        return handler.extractItem(slot, amount, simulate);
     }
 }
